0be625ca477ea575421f2eda26db2b14c9eb47a2
Tknott95 <tknott95@hotmail.com>
2018-02-25 00:55:50 -0700

    Arch Configs - Hardware Accel Drivers @TK

---
+++ ArchLinux_DevOps/hardware_accel_drivers.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# tk - tex - trevor
+echo "GRABBING YAOURT PKG's nvidia-utils libva-vdpau-driver libva-intel-driver   libvdpau-va-gl  libva-intel-driver-g45-h264 libva-intel-driver mesa libva-mesa-driver libva-vdpau-driver  amdgpu-pro-vdpau  libva-intel-driver-g45-h264 " 
+echo `yaourt -S --noconfirm nvidia-utils libva-vdpau-driver  libvdpau-va-gl libva-intel-driver mesa  libva-intel-driver-g45-h264 libva-intel-driver libva-mesa-driver libva-vdpau-driver  amdgpu-pro-vdpau  libva-intel-driver-g45-h264  `
