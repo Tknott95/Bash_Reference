0276dce55087bdb8e7ed3f9e5ca27231e2ad76a1
Tknott95 <tknott95@hotmail.com>
2018-02-25 01:44:32 -0700

    Arch PKG Management Scripts @TK

---
+++ ArchLinux_DevOps/env_var_refresg.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "SETTING ys GLOBAL variable\n"
+echo `export ys="yaourt -S --noconfirm "`
---
+++ ArchLinux_DevOps/printer_configs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# tk - trevor - tex
+echo "GRABBING yaourt -S cups brother-hl2140 samsung-unified-driver  a2ps  python-pyqt4 cups-pdf samba avahi foomatic-db-engine   foomatic-db-ppds  foomatic-db-nonfree-ppds  foomatic-db-gutenprint-ppds min12xxw ghostscript gutenprint gsfonts --noconfirm"
+echo `yaourt -S cups brother-hl2140 samsung-unified-driver  a2ps  python-pyqt4 cups-pdf samba avahi foomatic-db-engine foomatic-db-ppds  foomatic-db-nonfree-ppds  foomatic-db-gutenprint-ppds min12xxw ghostscript gutenprint gsfonts --noconfirm`
