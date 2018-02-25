a78cc943a829da38d32b28e2cfe5ac20aa74634b
Tknott95 <tknott95@hotmail.com>
2018-02-25 02:07:04 -0700

    Android Installer Emulator ++ @TK

---
+++ ArchLinux_DevOps/andorid_studio_installer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#  export JAVA_HOME=/usr/lib/jvm/java-version-openjdk
+echo "Android-sdk will be installed on /opt/android-sdk/ \n"
+echo `$ys lib32-gcc-libs git gnupg flex bison gperf sdl wxgtk2 squashfs-tools curl ncurses zlib schedtool perl-switch zip unzip libxslt python2-virtualenv bc rsync ncurses5-compat-libs  ncurses5-compat-libs  lib32-ncurses5-compat-libs`
+echo `$ys android-platform android-sdk android-sdk-platform-tools android-udev android-sdk-build-tools android-support  android-support-repository`
